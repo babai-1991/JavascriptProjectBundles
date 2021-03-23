34a4d2d6805a579cefc2afd1b8bf6f76c7625b89
Suvankar Das <iambabai0@gmail.com>
2021-03-23 13:07:18 +0530

    menu slider modal css js

---
+++ 5-ModalMenuSlider/script.js
@@ -0,0 +1,27 @@
+const toggleBtn = document.getElementById('toggle');
+const close = document.getElementById('close');
+const open = document.getElementById('open');
+const modal = document.getElementById('modal');
+
+//toggle navigation
+toggleBtn.addEventListener('click', () => {
+	document.body.classList.toggle('show-nav');
+});
+
+//show modal
+open.addEventListener('click', () => {
+	modal.classList.add('show-modal');
+});
+
+//hide modal
+close.addEventListener('click', () => {
+	modal.classList.remove('show-modal');
+});
+
+//hide modal on outside click
+window.addEventListener('click', (e) => {
+	if (e.target == modal) {
+		modal.classList.remove('show-modal');
+	}
+	return false;
+});
